70301ef9e237f37fbd0890c19b4117134a07f392
github-young <xymailmooc@163.com>
2020-01-06 02:59:29 +0800

    fix cnyhkd display

--- config_file/linux/scripts/cnyhkd.sh
+++ config_file/linux/scripts/cnyhkd.sh
@@ -1,3 +1,3 @@
 RES=$(curl -s https://finance.yahoo.com/quote/CNYHKD\=X/)
 echo "$RES" | grep -o -P ">1.{0,5}" | xargs | awk -F\> '{ print "100CNY = " 100*$2 "HKD" }'
-echo "$RES" | grep -o -P ".{0,15}%\)<" 
+echo "$RES" | grep -o -P ".{0,15}%\)<" | grep -o -P ">.{0,20}<"
