36f3c073b67197b77b871bdbbbda5025395688c6
github-young <xymailmooc@163.com>
2020-01-06 17:37:01 +0800

    update scanGithubName.sh

--- config_file/linux/scripts/scanGithubName.sh
+++ config_file/linux/scripts/scanGithubName.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-names='njudii github-young groupdu 54lH jiangsu'
+version=0.1
 
-for name in $names
-do
+inputNames=$*
+
+:<<EOF
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+  -V | --version )
+    echo $version
+    exit
+    ;;
+  -n | --name )
+    shift; inputNames+=$1
+    ;;
+esac; shift; done
+if [[ "$1" == '--' ]]; then shift; fi
+EOF
+
+#echo -n "Input a GitHub username: "
+#read inputNames
+
+#names='njudii github-young groupdu 54lH jiangsu'
+
+for name in $inputNames; do
   res=$(curl -Is "https://github.com/$name" | head -1 | awk '{print $2}')
   #printf "%s\t%d\n" $name $res
   echo -e "$res \t $name"
 done
 
+echo "Done."
