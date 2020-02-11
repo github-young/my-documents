5280c3effbcaac46f008ed2c222f7b2a03d6abbe
github-young <xymailmooc@163.com>
2020-02-11 16:33:32 +0800

    fix RES problem

--- config_file/linux/scripts/boc.sh
+++ config_file/linux/scripts/boc.sh
@@ -3,7 +3,8 @@
 #RES=$(curl -s https://www.boc.cn/sourcedb/whpj/ | grep -A 6 "<td>港币" | awk -F "</*td" '{ print $2 }' | awk -F ">" '{print $2}')
 #echo $RES | awk '{ printf "BOC-buy-in:\t%.2f (%.2f)\nBOC-middle:\t%.2f (%.2f)\nReleased-at:\t%s\n", $2, 10000/$2, $6, 10000/$6, $8 }'
 
-getEr=$(curl -s https://www.bankofchina.com/sourcedb/whpj/sjmfx_1621.html | grep -A 4 "HKD" | awk -F\<td\> '{ print $2}' | awk -F\<\/td\> '{ print $1 }' | xargs)
-releaseTime=$(curl -s https://www.bankofchina.com/sourcedb/whpj/sjmfx_1621.html | grep -A 1 "time" | sed -e '1d' | xargs | sed -e 's/<\/p>//g' | awk '{ print $2 }')
+RES=$(curl -s https://www.bankofchina.com/sourcedb/whpj/sjmfx_1621.html)
+getEr=$(echo "$RES" | grep -A 4 "HKD" | awk -F"<td>" '{ print $2}' | awk -F\<\/td\> '{ print $1 }' | xargs)
+releaseTime=$(echo "$RES" | grep -A 1 "time" | sed -e '1d' | xargs | sed -e 's/<\/p>//g' | awk '{ print $2 }')
 echo -e "Released-at:\t"$releaseTime
 echo $getEr | awk '{ printf "BOC-buy-in:\t%.2f (%.2f)\nBOC-sell-out:\t%.2f (%.2f)\n", $2, 10000/$2, $4, 10000/$4}'
