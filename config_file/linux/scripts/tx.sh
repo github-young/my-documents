9d0ac9b1d3778fa97ebec947db4b9519359de086
github-young <xymailmooc@163.com>
2020-02-25 16:16:44 +0800

    update tx

--- config_file/linux/scripts/tx.sh
+++ config_file/linux/scripts/tx.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cat "/sys/class/net/$1/statistics/tx_bytes" | awk '{ printf "%.2fMB\n", $1/1024.0/1024.0 }'
+
+_CAT=/usr/bin/cat
+_AWK=/usr/bin/awk
+
+$_CAT "/sys/class/net/$1/statistics/tx_bytes" | $_AWK '{ printf "%.2fMB\n", $1/1024.0/1024.0 }'
