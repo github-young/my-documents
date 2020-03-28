78324778f2b9749d7ec62c76b628e7c4e9647e31
github-young <xymailmooc@163.com>
2020-03-28 16:15:28 +0800

    update pullAll.sh echo gst

--- config_file/linux/scripts/pullAll.sh
+++ config_file/linux/scripts/pullAll.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-for i in ./**/.git/; do ( echo "[+] in ${i%\/\.git\/}"; cd $i/..; git pull; ); done
+for i in ./**/.git/; do ( echo "[+] in ${i%\/\.git\/}"; cd $i/..; git status -s; git pull; ); done
