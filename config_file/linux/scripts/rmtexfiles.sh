b116f97f3f1bd28070667383e27e283488626004
github-young <xymailmooc@163.com>
2020-04-29 15:23:21 +0000

    let texrm take arguments

--- config_file/linux/scripts/rmtexfiles.sh
+++ config_file/linux/scripts/rmtexfiles.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+
+echo "[+] Clear in path: $1"
+cd "$1"
+echo "[+] PWD is: $(pwd)"
+
 rm *.aux
 rm *.bbl
 rm *.bcf
 rm *.blg
+rm *.dvi
 rm *.fdb_latexmk
 rm *.fls
 rm *.log
 rm *.nav
+rm *.pdfsync
+rm *.ps
 rm *.run.xml
 rm *.snm
+rm *.synctex
 rm *.toc
 rm *.out
 rm *.gz
