916ccd198c9b53c20e886e6236e185aeda1f7027
github-young <xymailmooc@163.com>
2020-08-06 19:32:16 +0800

    update hotspot.sh

--- config_file/linux/scripts/hs.sh
+++ config_file/linux/scripts/hs.sh
@@ -2,6 +2,7 @@
 
 #echo "nmcli status is:"
 nmcli device status
+echo "==========================="
 
 if [[ "$1" == "on" ]]; then
   sudo nmcli device wifi hotspot ifname wlp0s26u1u1 con-name 4137 ssid 4137 band bg channel 8 password xykindle
@@ -9,4 +10,8 @@ fi
 
 if [[ "$1" == "off" ]]; then
   sudo nmcli device disconnect wlp0s26u1u1
+fi
+
+if [[ "$1" == "show" ]]; then
+  sudo nmcli dev wifi show-password
 fi
