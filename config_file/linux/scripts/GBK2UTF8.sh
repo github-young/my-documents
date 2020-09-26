039f3ce1f5f34447002e2aa57547aeb43ccb702c
github-young <xymailmooc@163.com>
2020-09-26 19:32:00 +0800

    update GBK2UTF8

--- config_file/linux/scripts/GBK2UTF8.sh
+++ config_file/linux/scripts/GBK2UTF8.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+_proceedFlag="N"
+
 echo "[+] PWD is: $(pwd)"
-echo "[+] Converting: $1"
-iconv -f GBK -tUTF-8 $1 > $1.bak
-mv $1.bak $1
-echo "[+] Done."
+echo "[+] Converting: $1. Proceed? [y/N]:"
+read _proceedFlag
+if [[ ${_proceedFlag} == "y" ]]; then
+  iconv -f GBK -t UTF-8 $1 > $1.bak
+  mv $1.bak $1
+  echo "[+] Done."
+else
+  echo "[-] Unchanged."
+fi
