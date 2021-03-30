b3f59f0d8967f1f0940cc69cdcb310ae5e7ba197
github-young <xymailmooc@163.com>
2021-03-30 16:29:07 +0800

    better output

--- config_file/linux/scripts/cnyhkd.sh
+++ config_file/linux/scripts/cnyhkd.sh
@@ -1,3 +1,3 @@
 RES=$(curl -s https://finance.yahoo.com/quote/CNYHKD\=X/)
-echo "$RES" | grep -o -E ">1\..{0,5}" | xargs | awk -F\> '{ printf "100HKD = %.2fCNY\n100CNY = %.2fHKD\n", 100/$2, 100*$2 }'
+echo "$RES" | grep -o -E ">1\..{0,5}" | xargs | awk -F\> '{ printf "100HKD = %6.2fCNY\n100CNY = %6.2fHKD\n", 100/$2, 100*$2 }'
 echo "$RES" | grep -o -E ".{0,15}%\)<" | grep -o -E ">.{0,20}<"
