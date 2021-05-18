46dff85cec7ad07f347747d4bb9b822a46af96a7
github-young <xymailmooc@163.com>
2021-05-18 19:17:25 +0800

    update tty banner

--- config_file/linux/scripts/banner.sh
+++ config_file/linux/scripts/banner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 MSG=$1
-sudo echo -e "\e[3J" > /dev/tty1
-sudo banner -w 200 -C -c "#" "${MSG} | $(date +"%H:%M")" > /dev/tty1
+echo -e "\e[3J" | sudo tee /dev/tty1 > /dev/null
+banner -w 200 -C -c "#" "${MSG} | $(date +"%H:%M")" | sudo tee /dev/tty1 > /dev/null
