7cdbe6211eae77862d01b076d23217565c367d4d
github-young <xymailmooc@163.com>
2021-05-21 13:18:36 +0800

    specify WDIR

--- config_file/linux/scripts/pullAll.sh
+++ config_file/linux/scripts/pullAll.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-_WDIR=/home/young/Documents/repos
+_WDIR=${HOME}/Documents/repos
 for i in ${_WDIR}/**/.git/; do ( echo "[+] in ${i%\/\.git\/}"; cd $i/..; git status -s; git pull; ); done
