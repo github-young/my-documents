551c7a763ee67a662c57f9cd0db0c34e3f7532c9
github-young <xymailmooc@163.com>
2021-05-31 11:49:03 +0800

    specify Weekday

--- config_file/linux/scripts/banner.sh
+++ config_file/linux/scripts/banner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 MSG=$1
 echo -e "\e[3J" | sudo tee /dev/tty1 > /dev/null
-banner -w 200 -C -c "#" "${MSG} | $(date +"%H:%M")" | sudo tee /dev/tty1 > /dev/null
+banner -w 200 -C -c "#" "${MSG} | $(date +"%a %H:%M")" | sudo tee /dev/tty1 > /dev/null
