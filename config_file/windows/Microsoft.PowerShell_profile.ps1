a955d4eb9cfc285674a86fca68a144eaf778fa1b
github-young <xymailmooc@163.com>
2023-06-30 12:36:20 +0800

    add powershell config

---
+++ config_file/windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+# Install oh-my-posh first
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/robbyrussell.omp.json" | Invoke-Expression
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Alias
+New-Alias -Name ll -Value Get-ChildItem -Force
+New-Alias -Name l -Value dir
+New-Alias -Name c -Value cls
+function ex {exit}
+New-Alias -Name e -Value ex
